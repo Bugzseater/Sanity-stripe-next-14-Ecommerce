51c69b045b77a2b0ea44f3f4995d62da46233e70
Pasindu <pasindumadushan.org@gmail.com>
2024-07-01 20:53:25 +0530

    up

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -2,39 +2,33 @@ import React from 'react';
 import '../app/globals.css';
 import Link from 'next/link';
 import Image from 'next/image';
+import { urlFor } from '../../lib/client';
 
-const HeroBanner = ({ heroBanner}) => {
+const HeroBanner = ({ heroBanner }) => {
   return (
     <div className='hero-banner-container'>
       <div>
         <p className='beats-solo'>{heroBanner.smallText}</p>
         <h3>{heroBanner.midText}</h3>
         
-        <Image
+          <Image
             src=''
             alt='headphone'
             className='hero-banner-image'
-            width={500}  // Set appropriate width
-            height={300} // Set appropriate height
-            layout='responsive'
           />
 
         <div>
-          <Link href="/product/ID">
-
+          <Link href={`/product/${heroBanner.productId}`}>
             <button type='button'>BUTTON TEXT</button>
-          
           </Link>
-
           <div className='desc'>
             <h5>Description</h5>
-            <p>DESCRIPTION</p>
+            <p>{heroBanner.description}</p>
           </div>
         </div>
       </div>
-      
     </div>
-  )
+  );
 }
 
-export default HeroBanner
+export default HeroBanner;
