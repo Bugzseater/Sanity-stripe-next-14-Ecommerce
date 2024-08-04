a84b2f2cebf2ace011d45a50bb25447d8843d466
Pasindu <pasindumadushan.org@gmail.com>
2024-08-04 23:43:23 +0530

    up

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,9 @@
 import Image from "next/image";
 import Login from "./login/page";
+import Main from "./main/page";
 
 export default function Home() {
   return (
-    <Login/>
+    <Main/>
   );
 }
